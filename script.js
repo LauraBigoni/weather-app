427f0fb4b55602627633df2f02cdc5ba02daf655
Laura Bigoni <laurabigoni.92@gmail.com>
2022-06-13 17:25:56 +0200

    uso il navigator per geolocalizzare

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log('JS OK!');
+
+// * Recupero tutti gli elementi dal DOM
+const weatherIcon = document.querySelector('.weather-icon');
+const weatherLocation = document.querySelector('.weather-location');
+const weatherTemperature = document.querySelector('.weather-temperature');
+const weatherWind = document.querySelector('.weather-wind');
+const weatherHumidity = document.querySelector('.weather-humidity');
+
+const rootElement = document.documentElement;
+
+// * Recupero la posizione
+window.navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+// * Funzione da eseguire in caso di errore di geolocalizzazione
+function onError(error) {
+  console.log('error');
+  weatherLocation.innerHTML = `<h3>Devi attivare la geolocalizazione</h3>`;
+}
+
+// * Funzione da eseguire in caso di successo
+function onSuccess(position) {
+  console.log('success', position);
+
+}
